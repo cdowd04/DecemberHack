4ecac1f26e1bda4ce224a0864c16b3dba77c8b05
cdowd04 <ciandowd@gmail.com>
2020-12-08 16:33:26 -0500

    Create script.py
    created main script with current time function

---
+++ script.py
@@ -0,0 +1,11 @@
+import time
+
+def main():
+    from datetime import datetime
+
+    now = datetime.now()
+
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+
+main()
