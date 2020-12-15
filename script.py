967ab5662b20d183452ce7f17729d8dd29b371f6
cdowd04 <ciandowd@gmail.com>
2020-12-15 12:19:31 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import time
+import webbrowser
 
 def main():
     from datetime import datetime
@@ -7,5 +8,11 @@ def main():
 
     current_time = now.strftime("%H:%M:%S")
     print("Current Time =", current_time)
+    print("Hello and good luck with school today")
+    meet1 = str(input("Please enter your first class meet link: "))
+    meet2 = input("Please enter your second class meet link: ")
+    meet3 = input("Please enter your third class meet link: ")
+    meet4 = input("Please enter your fourth class meet link: ")
+    meet5 = input("Please enter your fifth class meet link: ")
 
 main()
