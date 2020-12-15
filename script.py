5fa4affaacc7a32d73dc12a6a601263ff9d01a43
cdowd04 <ciandowd@gmail.com>
2020-12-15 14:36:44 -0500

    Updated script.py
    Changed print statements to be more clear.

--- script.py
+++ script.py
@@ -9,13 +9,13 @@ def getTime():
 
 def main():
     print("Good morning and welcome to google meet launcher!")
-    print(getTime())
+    print("The current time is: {0}".format(getTime()))
 
-    meet1 = input("Please enter your first class meet link: ")
-    meet2 = input("Please enter your second class meet link: ")
-    meet3 = input("Please enter your third class meet link: ")
-    meet4 = input("Please enter your fourth class meet link: ")
-    meet5 = input("Please enter your fifth class meet link: ")
+    meet1 = input("Please enter your first class meet link (make sure its the entire link): ")
+    meet2 = input("Please enter your second class meet link (make sure its the entire link): ")
+    meet3 = input("Please enter your third class meet link (make sure its the entire link): ")
+    meet4 = input("Please enter your fourth class meet link (make sure its the entire link): ")
+    meet5 = input("Please enter your fifth class meet link (make sure its the entire link): ")
 
     loop = True
     schedule = input("Is today a half day or full day? (Type \'h\' or \'f\' respectively): ")
@@ -33,6 +33,7 @@ def main():
                 webbrowser.open(meet4, new = 2)
             if (time == "13:14:00"):
                 webbrowser.open(meet5, new = 2)
+            if (time >= "13:15:00"):  
                 loop = False
     elif (schedule == 'h' or schedule =='H'):
         print("The program is now running and will open your google meet links at the right class schedule. Please do not close the program.")
@@ -48,8 +49,9 @@ def main():
                 webbrowser.open(meet4, new = 2)
             if (time == "11:40:00"):
                 webbrowser.open(meet5, new = 2)
-                loop = False    
+            if (time >= "11:41:00"):  
+                loop = False   
     else:
         print("EROR: You have entered an invalid response to the type of day schedule!!!")
-    print("Your school day is now over or you have entered an invalid response resulting in an error. The program will now end. Have a good day!")
+    print("You are now past your last period class or you have entered an invalid response. The program will now end. Have a good day!")
 main()
